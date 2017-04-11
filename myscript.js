843a1b08d9a91e3ef5bc304b30014b8b211a942f
connormueller <connor16@live.missouristate.edu>
2017-04-11 16:42:53 -0500

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -11,3 +11,25 @@ function validateForm(){
 
 var d = new Date();
 document.getElementById("date").innerHTML = d;
+
+var showing = false;
+
+function productMenu() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+
+
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn2')) {
+        
+        var dropdowns = document.getElementsByClassName("dropdown-content2");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
