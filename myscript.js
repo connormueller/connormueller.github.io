7321dbacc0d05d60a9c1573ef29642f3e49c4d09
connormueller <connor16@live.missouristate.edu>
2017-04-25 17:48:05 -0500

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -3,9 +3,7 @@ function validateForm(){
 	var b=document.forms["contactform"]["lastname"];
 	var c=document.forms["contactform"]["emailaddress"];
 
-	if (a=null || a="" || b=null || b="" || c=null || c="") {
-		alert("All fields need to be filled out");
-		return false;
+	if (a==null || a=="" || b==null || b=="" || c==null || c=="") {alert("All fields need to be filled out");return false;
 	}
 }
 
