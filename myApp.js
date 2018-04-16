ffd0ccc2fba39ec7628bbab4f1e90cceaf2cd111
connormueller <connor16@live.missouristate.edu>
2018-04-16 14:08:59 -0500

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -12,21 +12,21 @@ app.controller('activitiesCtrl', [
               { title: 'Sports', activity: 'Brisbane' },
               { title: 'Cycling', activity: 'Brisbane' },
               { title: 'Cycling', activity: 'Berlin' },
-              { title: 'Museums', activity: 'Brisbane' }
-              { title: 'Museums', activity: 'Vancouver' }
-              { title: 'Museums', activity: 'New York City' }
-              { title: 'Museums', activity: 'Berlin' }
-              { title: 'Boating', activity: 'Brisbane' }
-              { title: 'Boating', activity: 'Vancouver' }
-              { title: 'Boating', activity: 'Cancun' }
-              { title: 'Sailing', activity: 'Vancouver' }
-              { title: 'Beach', activity: 'Vancouver' }
-              { title: 'Beach', activity: 'Cancun' }
-              { title: 'Hiking', activity: 'Vancouver' }
-              { title: 'Theatre', activity: 'New York City' }
-              { title: 'Parks and Recreation', activity: 'New York City' }
-              { title: 'Parks and Recreation', activity: 'Cancun' }
-              { title: 'Snorkeling', activity: 'Cancun' }
+              { title: 'Museums', activity: 'Brisbane' },
+              { title: 'Museums', activity: 'Vancouver' },
+              { title: 'Museums', activity: 'New York City' },
+              { title: 'Museums', activity: 'Berlin' },
+              { title: 'Boating', activity: 'Brisbane' },
+              { title: 'Boating', activity: 'Vancouver' },
+              { title: 'Boating', activity: 'Cancun' },
+              { title: 'Sailing', activity: 'Vancouver' },
+              { title: 'Beach', activity: 'Vancouver' },
+              { title: 'Beach', activity: 'Cancun' },
+              { title: 'Hiking', activity: 'Vancouver' },
+              { title: 'Theatre', activity: 'New York City' },
+              { title: 'Parks and Recreation', activity: 'New York City' },
+              { title: 'Parks and Recreation', activity: 'Cancun' },
+              { title: 'Snorkeling', activity: 'Cancun' },
             ]
         }
     }
